7ee59c34606da4628d8586e7ae98e2f3c1276a40
KGH <kghui20@gmail.com>
2024-08-26 16:57:51 +0900

    WAS - 요청마다 별도의 처리 스레드 생성

--- README.md
+++ README.md
@@ -31,3 +31,14 @@ GET http://localhost:8080/calculate?operand1=10&operator=*&operand2=55
 ###
 GET http://localhost:8080/calculate?operand1=10&operator=/&operand2=55
 ***
+
+
+### 각 상황별 문제점
+#### 메인스레드에서만 실행 시 :
+요청 발생시 작업을 수행하며 block, 다음 클라이언트의 요청은 이전 요청이 끝날 때까지 대기해야함.
+<br><br>
+#### 별도의 스레드에서 추가 실행 시:
+요청 발생마다 새로운 스레드를 생성하여 작업을 수행하며, 이는 새로운 Stack메모리 공간을 할당 받게됨. 따라서 높은 리소스 소비가 요구된다.
+<br><br>
+
+### Thread Pool을 적용하거나 비동기 처리를 통해 완화할 수 있다.
--- src/main/java/com/practice/CustomWebApplicationServer.java
+++ src/main/java/com/practice/CustomWebApplicationServer.java
@@ -29,34 +29,8 @@ public class CustomWebApplicationServer {
 			while((clientSocket = serverSocket.accept())  != null) {
 				logger.info("{CustomWebApplicationServer} client connected!");
 
-				try(InputStream in = clientSocket.getInputStream(); OutputStream out = clientSocket.getOutputStream()) {
-					BufferedReader reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-					DataOutputStream dos = new DataOutputStream(out);
-//
-//					String line;
-//					while((line = reader.readLine()) != "") {
-//						System.out.println(line);
-//					}
+				new Thread(new ClientRequestHandler(clientSocket)).start();
 
-					HttpRequest httpRequest = new HttpRequest(reader);
-					//GET /calculate?operand1=11&operator=*&operand2=55 HTTP/1.1
-
-					if(httpRequest.isGetRequest() && httpRequest.matchPath("/calculate")) {
-						QueryStrings queryStrings = httpRequest.getQueryStrings();
-
-						int operand1 = Integer.parseInt(queryStrings.getValue("operand1"));
-						String operator = queryStrings.getValue("operator");
-						int operand2 = Integer.parseInt(queryStrings.getValue("operand2"));
-
-						int calculated = Calculator.calculate(new PositiveValidation(operand1), operator , new PositiveValidation(operand2));
-						byte[] body = String.valueOf(calculated).getBytes();
-
-						HttpResponse response = new HttpResponse(dos);
-						response.response200Header("application/json", body.length);
-						response.responseBody(body);
-					}
-
-				}
 			}
 		}
 
